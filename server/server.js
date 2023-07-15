ec80f5980706b5a8e28bb00b56c9ec40910eaa5c
valentin-llv <lelievre.valentin17@gmail.com>
2023-07-16 00:45:24 +0200

    Patch get today midnight

--- server/server.js
+++ server/server.js
@@ -497,7 +497,10 @@ function isDateYesterday(dateTimestamp) {
 
 function getTodayMidnight() {
     let date = new Date();
-    date.setUTCHours(0, 0, 0, 0);
+    date.setHours(0);
+    date.setMinutes(0);
+    date.setSeconds(0);
+    date.setMilliseconds(0);
 
     return date;
 }
